e3f4804a447f9835703e9d5feb1440eb3847454b
Guido84444 <guido.marcellino@gmail.com>
2026-07-26 00:32:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GymBro from './App.jsx'
+import GymBro from './app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
